ca4c139fca78f666b1ed34933cac3ee2f2ffd9f1
Cattani <contato.cattani@gmail.com>
2021-06-25 02:05:11 -0300

    install active model serialier and creates user serializer

--- Gemfile
+++ Gemfile
@@ -13,6 +13,7 @@ gem 'puma', '~> 5.0'
 # Use Active Model has_secure_password
 gem 'bcrypt', '~> 3.1.7'
 gem 'jwt'
+gem 'active_model_serializers', '~> 0.10.0'
 
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.4', require: false
---
+++ spec/serializers/user_serializer_spec.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe UserSerializer, type: :serializer do
+  let(:user) { create(:user) }
+  subject(:serializer) { described_class.new(user).as_json }
+  
+  it 'returns a user serialized' do
+    expect(serializer[:name]).to eq(user.name)
+    expect(serializer[:email]).to eq(user.email)
+  end
+end
